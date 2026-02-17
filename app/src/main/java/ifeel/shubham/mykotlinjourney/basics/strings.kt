f396c71032a65343e88c7a7d34a9727646d4cd59
Shubham Raj <ershubhcsofficial@gmail.com>
2026-02-17 13:46:29 +0530

    String Interpolation

---
+++ app/src/main/java/ifeel/shubham/mykotlinjourney/basics/strings.kt
@@ -0,0 +1,16 @@
+package ifeel.shubham.mykotlinjourney.basics
+
+fun main() {
+    // string interpolation
+    var name = "Shubham"
+    val age = 20
+    name = "Raj"
+    println("Hi $name, how are you doing?")
+    println("And you are ${age+1}.")
+}
+
+// what is happening here ->
+// name variable is created and it points to "Shubham", alloted in the memory
+// when updated to "Raj"
+// New string "Raj" is created
+// name variable is made to point Raj
---
+++ app/src/main/java/ifeel/shubham/mykotlinjourney/basics/testsfile.kt
@@ -0,0 +1,11 @@
+package ifeel.shubham.mykotlinjourney.basics
+
+fun main() {
+    val num1 = 5
+    val s1 = "Jerry"
+
+    println("num1 = $num1")
+    println("s1 = $s1")
+
+    println("✅ Running")
+}
